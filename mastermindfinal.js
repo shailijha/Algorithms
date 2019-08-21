9f2e64a53dce1c1e4304a737844fca6e97ab58d6
shailijha <shaili_jha@yahoo.co.in>
2019-08-20 20:44:32 -0400

    Solution comment added

--- mastermindfinal.js
+++ mastermindfinal.js
@@ -9,8 +9,12 @@ the number of guesses
 2.1 return true if the guess is correct
 Ask the answer for 1014 and 2315*/
 
-const guess = "1122";
-const code = "1212";
+
+//soluttion - check if there is a num matching it above that character. if yes, then skip that char coz you are going
+//to encounter it later on.
+
+const guess = "1014";
+const code = "2315";
 
 let count_char = 0;
 let count_correct_posn = 0;
@@ -21,26 +25,32 @@ let cp_map = new Map();
 function masterMind(guess,code) {
   let guess_split = guess.split('');
   let code_split = code.split('');
-  console.log(`guess_split: ${guess_split}, code_split: ${code_split}`);
+  //console.log(`guess_split: ${guess_split}, code_split: ${code_split}`);
   for(let num = 0; num < guess_split.length; num ++) {
     for(let num1 = 0; num1 < code_split.length; num1++) {
       if(guess_split[num] === code_split[num1]) {
         if(num !== num1) {
-          console.log(cp_map.has(code_split[num1]));
-          if(!cc_map.has(code_split[num1]) && !cp_map.has(code_split[num1])) {
+          //console.log(cp_map.has(code_split[num1]));
+          if(cc_map.has(code_split[num1])) {
+            guess_split[num] = '';
+          }
+          else if(!cc_map.has(code_split[num1]) && !cp_map.has(code_split[num1])) {
             count_char += 1;
             cc_map.set(code_split[num1], count_char);
             guess_split[num] = '';
-            console.log(`POSITION DOESN'T MATCH IF guess_split: ${guess_split}, code_split: ${code_split}`);
+            console.log(`POSITION DOESN'T MATCH IF guess_split: ${guess_split} code_split: ${code_split}`);
             console.log(cc_map);console.log(cp_map);
             console.log();
+          }
+          else if(cc_map.has(code_split[num1]) && !cp_map.has(code_split[num1]) && num > num1) {
+            code_split[num1] = '';
           }
           else if(cc_map.has(code_split[num1]) && cp_map.has(code_split[num1])) {
             let temp_count_char = cc_map.get(code_split[num1]);
             temp_count_char += 1;
             cc_map.set(code_split[num1], temp_count_char);
             guess_split[num] = '';
-            console.log(`POSITION DOESN'T MATCH ELSE IF guess_split: ${guess_split}, code_split: ${code_split}`);
+            console.log(`POSITION DOESN'T MATCH ELSE IF guess_split: ${guess_split} code_split: ${code_split}`);
             console.log(cc_map);console.log(cp_map);
             console.log();
           }
@@ -53,15 +63,19 @@ function masterMind(guess,code) {
             cc_map.set(code_split[num1], count_char);
             cp_map.set(code_split[num1], count_correct_posn);
             code_split[num1] = ''; guess_split[num] = '';
-            console.log(`POSITION MATCHES IF guess_split: ${guess_split}, code_split: ${code_split}`);
+            console.log(`POSITION MATCHES IF guess_split: ${guess_split} code_split: ${code_split}`);
             console.log(cc_map);console.log(cp_map);
             console.log();
           }
           else if(cc_map.has(code_split[num1]) && !cp_map.has(code_split[num1])) {
             count_correct_posn += 1;
             cp_map.set(code_split[num1], count_correct_posn);
+            let temp_count_char = cc_map.get(code_split[num1]);
+            if(temp_count_char == cp_map.get(code_split[num1])) {
+                cc_map.set(code_split[num1], temp_count_char+=1);
+            }
             code_split[num1] = ''; guess_split[num] = '';
-            console.log(`POSITION MATCHES ELSE IF 1 guess_split: ${guess_split}, code_split: ${code_split}`);
+            console.log(`POSITION MATCHES ELSE IF 1 guess_split: ${guess_split} code_split: ${code_split}`);
             console.log(cc_map);console.log(cp_map);
             console.log();
           }
@@ -70,10 +84,10 @@ function masterMind(guess,code) {
             let temp_count_posn = cp_map.get(code_split[num1]);
             temp_count_char += 1;
             temp_count_posn += 1;
-            cc_map.set(code_split[num1], temp_count_char)
+            cc_map.set(code_split[num1], temp_count_char);
             cp_map.set(code_split[num1], temp_count_posn);
             code_split[num1] = ''; guess_split[num] = '';
-            console.log(`POSITION MATCHES ELSE IF 2 guess_split: ${guess_split}, code_split: ${code_split}`);
+            console.log(`POSITION MATCHES ELSE IF 2 guess_split: ${guess_split} code_split: ${code_split}`);
             console.log(cc_map);console.log(cp_map);
             console.log();
           }
