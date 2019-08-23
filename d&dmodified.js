2a6328ee59736db225d4f7de2538f7a67a23ff21
shailijha <shaili_jha@yahoo.co.in>
2019-08-23 12:23:27 -0400

    Final modified version of D&D

--- d&dmodified.js
+++ d&dmodified.js
@@ -1,9 +1,9 @@
 /*Write a separate class for attack since it is not a property of the dice.
 The constructor of Dice class can be modified to set the number of sides*/
 
-//var readlineSync = require('readline-sync');
+var readlineSync = require('readline-sync');
 
-//var modifier = readlineSync.question('Please provide a modifier ');
+var modifier = readlineSync.question('Please provide a modifier ');
 const min = 1;
 
 class Dice {
@@ -21,38 +21,38 @@ class Dice {
 }
 
 class Attack {
+  //static function that determines if the dice roll is a critical hit or miss
   static detectCritical(dice) {
-    //console.log(dice.diceRoll);
     return dice.diceRoll === 1 ? ('Critical Miss') : (dice.diceRoll === 20 ? 'Critical Hit' : 'Continue');
   }
 
+  //static function that determines if the dice roll is a normal hit or miss
   static determineHit(dice, enemyAC) {
-    //console.log(dice.diceRoll+parseInt(modifier));
     let newDiceRoll = dice.diceRoll+parseInt(modifier);
     return newDiceRoll <= enemyAC ? ('Miss') : ('Hit');
   }
 
+  //static function to calculate weapon damage
   static calculateDamage(weaponDamage){
     weaponDamage = weaponDamage.split('');
     let times = weaponDamage[0]; let side = weaponDamage[2];
     let hitDice = new Dice(side);
-    console.log(times, side, hitDice);
     let result = 0;
     for(let i=0;i<times;i++) {
-      console.log('Result before attack ',result);
       let temp = hitDice.rollDice();
-      console.log('temp is ',temp);
+      //console.log(`temp roll is ${temp}`);
       result += temp;
-      console.log('Result after attack ',result);
     }
-    console.log(`You got to Hit and your damage is ${result}`);
-    //console.log(weaponDamage);
+    return result;
   }
 }
 
 var sample = new Dice(20);
+sample.rollDice();
+//console.log(`The roll of the ${sample.sides}-sided dice is ${sample.rollDice()}`);
+console.log(sample);
 
-var test = {
+/*var test = {
   sides: 20,
   diceRoll: 20
 };
@@ -70,43 +70,31 @@ console.log(`Determine Hit is ${Attack.determineHit(sample,enemyAC)}`);
 if(Attack.determineHit(sample,enemyAC) === 'Hit') {
   let weaponDamage = '2d6';
   Attack.calculateDamage(weaponDamage)
-}
+}*/
 
-/*var criticalState = sample.detectCritical();
+var criticalState = Attack.detectCritical(sample);
 
 switch (criticalState) {
   case 'Critical Miss':
-    console.log('Critical Miss')
+    console.log('Unfortunately you got a Critical Miss');
     break;
   case 'Critical Hit':
     let weaponDamage = readlineSync.question('Please provide weapon damage ');
-    weaponDamage = weaponDamage.split('');
-    let times = weaponDamage[0]; let sides = weaponDamage[2];
-    //console.log(weaponDamage);
+    console.log(`You got to Critical Hit and your damage is ${Attack.calculateDamage(weaponDamage)}`);
     break;
   case 'Continue':
     var enemyAC = readlineSync.question('Please provide enemy attack class value ');
-    let hitOrMiss = sample.determineHit(enemyAC);
+    let hitOrMiss = Attack.determineHit(sample,enemyAC);
     console.log(hitOrMiss)
     if(hitOrMiss === 'Hit') {
       let weaponDamage = readlineSync.question('Please provide weapon damage ');
-      weaponDamage = weaponDamage.split('');
-      let times = weaponDamage[0]; let sides = weaponDamage[2];
-      let hitDice = new Dice(0);
-      let result = 0;
-      for(let i=0;i<times;i++) {
-        result += hitDice.rollDice(1,sides);
-      }
-      console.log(`You got to Hit and your damage is ${result}`);
-      //console.log(weaponDamage);
+      console.log(`You got to Hit and your damage is ${Attack.calculateDamage(weaponDamage)}`);
     }
     else {
-      console.log('Unfortunately you missed');
+      console.log('Unfortunately you missed your turn');
     }
     break;
   default:
-    console.log('Default');
+    console.log('Invalid case');
     break;
-}*/
-
-//console.log("2d6".split(''));
+}
