8ad88c3c78fd037259d6318d82dbdc42ef2af081
shailijha <shaili_jha@yahoo.co.in>
2019-08-27 13:33:59 -0400

    Added Player class

--- deckracer.js
+++ deckracer.js
@@ -5,6 +5,7 @@ or greater than the number. The  player to reach there first is the winner*/
 
 //Create classes for dice and card.Deck class should have function for shuffling, creation and layout of the
 //cards
+const minDiceRoll = 1;
 class Dice {
   //constructor to initialize the dice. Refactored from simpled&d
   constructor(value) {
@@ -14,13 +15,16 @@ class Dice {
 
   //function to roll an n-sided dice.  Refactored from simpled&d
   rollDice() {
-    let max = this.sides;
-    return this.diceRoll = Math.floor(Math.random() * (+max - +minDiceRoll)) + +minDiceRoll;
+    let max = this.sides+1;
+    //console.log(max);
+    let random = Math.floor(Math.random() * Math.floor(max));
+    random = (random === 0 ? 1: random);
+    //console.log(random);
+    return this.diceRoll = random;
   }
 }
 
 class Deck {
-
   //function to create the deck. Refactored from best hand algo
   static createDeck() {
     for (let i = 0; i < 4; i++) {
@@ -65,13 +69,8 @@ class Deck {
   static checkValidity(card,index) {
     if(Deck.checkValid(card.value)) {
       let randIdx = Deck.generateRandomCardIndex();
-      //console.log(`randIdx in firstCard ${randIdx}`);
-      //console.log('deck[randIdx] in first card');
-      //console.log(deck[randIdx].value);
-      //console.log(Deck.checkValid(deck[randIdx].value));
       while(randIdx === index && Deck.checkValid(deck[randIdx].value)) {
         randIdx = Deck.generateRandomCardIndex();
-        //console.log(`randIdx in while firstCard ${randIdx}`)
       }
       console.log(`values before swapping in ${index} card`);
       console.log(deck[index],deck[randIdx]);
@@ -107,31 +106,75 @@ class Deck {
 
   //function to pick random card index
   static generateRandomCardIndex() {
-    return Math.floor(Math.random() * (+max - +min)) + +min;
+    let random = Math.floor(Math.random() * Math.floor(maxCard));
+    return random;
+    //return Math.floor(Math.random() * (+max - +min)) + +min;
+  }
+}
+
+//Player class
+class Player {
+  //constructor to initialize current_position, playerDiceRoll and i.
+  //current_position stops at the place where playerDiceRoll <= card.value.
+  //i is needed since the next round will start from the next card and not from the beginning
+  constructor() {
+    this.current_position = -1;
+    this.playerDiceRoll = -1;
+    this.i = 0;
+  }
+
+  /*function for navigation of player. Player rolls dice. checks to see if the roll value is less
+  than or equal to deck[0].value. if yes, then sets current position to 0 and increments i. Else,
+  runs a while loop until it reaches a card that has a higher value than the roll*/
+  navigate(player) {
+    console.log('in player navigate function');
+    this.playerDiceRoll = sixSidedDice.rollDice();
+    //console.log('dice roll ',this.playerDiceRoll);
+    console.log(player);
+    console.log('values before starting');
+    console.log('1st value in the deck');
+    console.log(deck[this.i].value);
+    console.log('while condition check');
+    console.log(this.playerDiceRoll > deck[this.i].value)
+    if(this.playerDiceRoll == deck[this.i].value || this.playerDiceRoll < deck[this.i].value) {
+      this.current_position = this.i;
+      this.i += 1;
+      console.log(player);
+    }else {
+      while(this.playerDiceRoll > deck[this.i].value && this.i < 28) {
+        if(this.playerDiceRoll == deck[this.i].value) {
+          break;
+        }
+        console.log('deck[i] value in while loop');
+        console.log(deck[this.i].value);
+        this.i += 1;
+        this.current_position = this.i;
+        console.log('player');console.log(player);
+      }
+    }
   }
 }
 
 var deck = new Array();
 var sixSidedDice = new Dice(6);
 
-//constants for minimum and maximum values of Card and minimum roll of Dice
+//constants for minimum and maximum values of Card, invalid edge cards and minimum roll of Dice
 const minCard = 0;
-const maxCard = 27;
-const minDiceRoll = 1;
+const maxCard = 28;
+const invalidCards = [1,2,6,7];
 
-console.log(sixSidedDice);
-console.log(sixSidedDice.rollDice());
-console.log(sixSidedDice);
-
-let invalidCards = [1,2,6,7];
-
-//console.log(deck);
-/*Deck.createDeck();
-//console.log(deck);
+Deck.createDeck();
 Deck.shuffle(deck);
 console.log('shuffle deck');
 console.log(deck);
 Deck.checkDeck();
 console.log('check deck');
-console.log(deck);*/
-//console.log(deck.length);
+console.log(deck);
+
+let player1 = new Player();
+while(player1.current_position!=27) {
+    player1.navigate(player1);
+}
+/*console.log(sixSidedDice);
+console.log(sixSidedDice.rollDice());
+console.log(sixSidedDice);*/
