b53367c0e1ed1040817b21eae521279db1d7e37a
shailijha <shaili_jha@yahoo.co.in>
2019-08-31 16:50:28 -0400

    Tested and working V2.0 and V2.1

--- mastermindnew.js
+++ mastermindnew.js
@@ -8,21 +8,21 @@ correct position
 the number of guesses
 2.1 return true if the guess is correct*/
 
-const guess = '1122';
+const guess = '1014';
 const code = '1212';
 
 function masterMind(guess,code) {
-  let char_count = 0;
-  let posn_count = 0;
+  /*let char_count = 0;
+  let posn_count = 0;*/
+  let count = [0,0];
   let guess_split = guess.split('');
   let code_split = code.split('');
+
   for(let i = 0; i < guess_split.length; i++) {
     for(let j = 0; j < code_split.length; j++) {
-      //console.log(`i and j is ${i},${j}. chars compared are ${guess_split[i]},${code_split[j]}`);
-      //console.log(guess_split, code_split);
       if(guess_split[i] == code_split[j] && i == j && guess_split[i]!='') {
-        char_count++;
-        posn_count++;
+        count[0]++;
+        count[1]++;
         guess_split[i] = code_split[j] = '';
       }
       else if(guess_split[i] == code_split[j] && (i < j || i > j) && guess_split[i]!='') {
@@ -30,18 +30,41 @@ function masterMind(guess,code) {
           guess_split[i] = '';
         }
         else {
-          char_count++;
+          count[0]++;
           guess_split[i] = code_split[j] = '';
         }
       }
-
-      //console.log(`char_count and posn_count is ${char_count},${posn_count}`);
-      //console.log();
     }
   }
-  console.log(char_count);
-  console.log(posn_count);
-  console.log(guess_split, code_split);
+  return count;
 }
 
-masterMind(guess,code);
+function playGame() {
+  let num_guesses = 10;
+  let guess_num = 0;
+  let flag = true;
+  console.log(`The code is comprised of 4 numerical characters.You have ${num_guesses} guesses.`)
+  while(num_guesses > 0 && flag) {
+    if(num_guesses == 0) {
+      break;
+    }
+    var readlineSync = require('readline-sync');
+    let guess = readlineSync.question(`Please enter your guess `);
+    let result = masterMind(guess,code);
+    if(result[0] == 4 && result[1] == 4) {
+      flag = false;
+      console.log(`Congratulations! :) You won the game in ${guess_num+1} turn(s)`)
+      break;
+    }
+    num_guesses--;
+    guess_num++;
+    console.log(`You got ${result[0]} character(s) right.You got ${result[1]} character(s) right in the right position`);
+    console.log(`You have ${num_guesses} guesses left`);
+    console.log();
+  }
+  if(num_guesses == 0) {
+    console.log('You are out of guesses. You lost :(');
+  }
+}
+
+playGame();
