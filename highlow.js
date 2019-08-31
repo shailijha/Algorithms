29264190d82910983a53b849ac078218d3f2cd2f
shailijha <shaili_jha@yahoo.co.in>
2019-08-31 13:28:14 -0400

    Tested and working V 2.0

--- highlow.js
+++ highlow.js
@@ -3,11 +3,8 @@
 1.1 randomly pick the next card
 1.2 update the current card to have a suit and value
 1.3 update the randomizer to pick a suit as well
-1.4 ensure the next card does not match the current card for suit and value
-2.0 create an encompassing function to play the game
-2.1 ask user for high or low and print result
-2.2 keep track of the number of the correct guesses
-*/
+1.4 ensure the next card does not match the current card for suit and value*/
+
 function randomNumber(min, max) {
   return (Math.floor(Math.random() * (max - min + 1)) + min)
 }
@@ -156,21 +153,45 @@ function generateNextCard(current_card) {
   return next_card;
 }
 
+/*2.0 create an encompassing function to play the game
+2.1 ask user for high or low and print result
+2.2 keep track of the number of the correct guesses*/
+
 //version 2.0
 function highLowv6(guess) {
-  console.log(guess);
-
+  let flag = true;
   let current_card = {
     suit: suits[randomNumber(0,3)],
     value: randomNumber(2,14)
   };
 
-  let next_card = {
-    suit: '',
-    value: 0
-  };
+  let next_card = generateNextCard(current_card);
+  console.log('current card v2.0 is ',current_card);
+  console.log('next card in v2.0 ',next_card);
+  console.log('guess is ',guess);
 
-
+  if(current_card.value == next_card.value) {
+    return "draw";
+  }
+  else if(current_card.value < next_card.value && guess == "low" || current_card.value > next_card.value && guess == "high") {
+    return "lose"
+  }
+  else {
+    while(flag) {
+      //console.log('guess is ',guess);
+      if(current_card.value < next_card.value && guess == "high" || current_card.value > next_card.value && guess == "low") {
+        flag = true;
+      }
+      else {
+        flag = false;
+        return "You lose";
+      }
+      current_card = next_card;
+      next_card = generateNextCard(current_card);
+      console.log('current_card = next_card ',current_card);
+      console.log('new next card ',next_card);
+    }
+  }
 }
 
 let next_card = 'J';
@@ -187,6 +208,6 @@ let guess = 'high';
 //console.log('card', current_card);
 //console.log(highLowv4(current_card,guess));
 //console.log('card', current_card);*/
-console.log(highLowv5(current_card,guess));
+//console.log(highLowv5(current_card,guess));
 //console.log('card', current_card);
-//highLowv6('low');
+console.log(highLowv6('high'));
