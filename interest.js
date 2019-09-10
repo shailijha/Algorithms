33862c3ad1545af8f7bb5d5ea95d63bf38a9249c
shailijha <shaili_jha@yahoo.co.in>
2019-09-10 14:05:24 -0400

    Added function for Withdraw and withdraw occurence

--- interest.js
+++ interest.js
@@ -37,13 +37,14 @@ function calculateInterestv3(bal,time, interest, deposit, dep_occur) {
     new_bal *= 100;
     new_bal = Math.floor(new_bal);
     new_bal /= 100;
-    console.log('new_bal ',new_bal);
-    if(i % dep_occur) {
+    //console.log('new_bal ',new_bal);
+    if((i+1) % dep_occur == 0 && i > 0) {
+      //console.log(i, dep_occur-1);
       new_bal += deposit;
     }
 
     bal = new_bal;
-    console.log('bal ',bal);
+    //console.log('bal ',bal);
   }
 
   return new_bal;
@@ -54,22 +55,29 @@ function calculateInterestv4(bal,time, interest, deposit, dep_occur, wthdrw, wth
   let time_in_months = time * 12;
   let new_bal = 0;
   for(let i = 0; i < time_in_months; i++) {
+    if((i+1) % wthdrw_occur == 0 && i > 0) {
+      //console.log('withdraw ',i+1,wthdrw_occur);
+      bal -= wthdrw;
+    }
+    //console.log('bal after withdraw ',bal);
     new_bal = bal + ((bal * interest) / 100);
     new_bal *= 100;
     new_bal = Math.floor(new_bal);
     new_bal /= 100;
-    console.log('new_bal ',new_bal);
-    if(i % dep_occur) {
+    //console.log('after interest ',new_bal);
+    if((i+1) % dep_occur == 0 && i > 0) {
+      //console.log('deposit ',i+1,dep_occur);
       new_bal += deposit;
     }
 
     bal = new_bal;
-    console.log('bal ',bal);
+    //console.log('final bal ',bal);
   }
 
   return new_bal;
 }
 
-//console.log(calculateInterestv1(100,1,1));
-//console.log(calculateInterestv2(100,1,1,100));
+console.log(calculateInterestv1(100,1,1));
+console.log(calculateInterestv2(100,1,1,100));
 console.log(calculateInterestv3(100,1,1,100,6));
+console.log(calculateInterestv4(1000,1,1,500,6,50,2));
